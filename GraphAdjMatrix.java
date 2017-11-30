dd8466ceb03b7f03876fe78b75c7475137942bd2
Kelly Heaney <31257783+kheaney21@users.noreply.github.com>
2017-11-29 23:44:03 -0800

    Create GraphAdjMatrix.java

---
+++ GraphAdjMatrix.java
@@ -0,0 +1,67 @@
+public class GraphAdjMatrix implements Graph {
+	
+	private int vertices;
+	private int[][] graph;
+	
+	public GraphAdjMatrix(int numVertices) {
+		vertices = numVertices;
+		graph = new int[vertices][vertices];
+	}
+	
+	public void addEdge(int src, int tar) {
+		graph[src][tar] = 1;
+	}
+	
+	public int[] neighbors(int vertex) {
+		int[] temp = new int[vertices]; 
+		int index = 0;
+		
+		for(int i = 0; i < graph.length; i++) {
+			if (graph[vertex][i] == 1) {
+					temp[index] = i;
+					index++;
+			}
+		}
+		return temp;
+	}
+	
+	public void topologicalSort() {
+		
+		int[] temp = new int[vertices];
+		boolean[] visited = new boolean[vertices];
+		int index = 0;
+		int count;
+		int min;
+		
+		for(int i = 0; i < graph.length; i++) { //column
+			count = 0;
+			for(int j = 0; j < graph.length; j++) { //row
+				if (graph[j][i] == 1) {
+					count++;
+				}
+			}
+			temp[i] = count;
+		}
+		
+		for (int k = 0; k < temp.length; k++) {
+			min = Integer.MAX_VALUE;
+			index = 0;
+			for (int m = 0; m < temp.length; m++) {
+				if (temp[m] < min && visited[m] != true) {
+					min = temp[m];
+					index = m;
+				}
+			}
+			System.out.println(temp[index]);
+		}
+		
+	}
+	
+	public int growArray(int[] arr, int size) {
+		int[] newArr = new int[size * 2];
+		System.arraycopy(arr, 0, newArr, 0, size);
+		arr = newArr;
+		size = size * 2;
+		return size;
+	}
+}
